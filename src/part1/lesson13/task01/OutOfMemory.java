95409bf244a52c36b0065cfc5024c023944a21d4
Shakhriyor Ergashev <shahriyor.ergashev@gmail.com>
2021-06-26 00:08:39 +0500

    assignment 13 is done

---
+++ src/part1/lesson13/task01/OutOfMemory.java
@@ -0,0 +1,26 @@
+package part1.lesson13.task01;
+
+public class OutOfMemory {
+    public static void main(String[] args) throws InterruptedException {
+
+
+        int arrSize = 15;
+        System.out.println("Maximum memory " + Runtime.getRuntime().maxMemory());
+        long memoryConsumed = 0;
+        try {
+            long[] memoryAllocated = null;
+            for (int loop = 0; loop < Integer.MAX_VALUE; loop++) {
+                memoryAllocated = new long[arrSize];
+                memoryAllocated[0] = 0;
+                memoryConsumed += arrSize * Long.SIZE;
+                System.out.println("Memory Consumed: " + memoryConsumed);
+                arrSize *= arrSize * 2;
+                Thread.sleep(500);
+            }
+        } catch (OutOfMemoryError outofMemory) {
+            System.out.println("Catching out of memory error");
+            throw outofMemory;
+        }
+
+    }
+}
